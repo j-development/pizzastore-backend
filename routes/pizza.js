50bbf2555d032e3e4d1c2adfe702a3c5c37d0ec9
ekdahl1 <johan.ekdahl96@gmail.com>
2022-09-25 19:50:42 +0200

    added Ids

--- routes/pizza.js
+++ routes/pizza.js
@@ -13,71 +13,85 @@ const pizzaGroup = {
 
 let pizzaList = [
   {
+    id: 1,
     name: 'MARGARITA',
     price: 80,
     pizzagroup: 1,
   },
   {
+    id: 2,
     name: 'VESUVIO',
     price: 85,
     pizzagroup: 1,
   },
   {
+    id: 3,
     name: 'CAPRICCIOSA',
     price: 90,
     pizzagroup: 2,
   },
   {
+    id: 4,
     name: 'SALAMI',
     price: 90,
     pizzagroup: 2,
   },
   {
+    id: 5,
     name: 'TOMASO',
     price: 90,
     pizzagroup: 2,
   },
   {
+    id: 6,
     name: 'HAWAII',
     price: 90,
     pizzagroup: 2,
   },
   {
+    id: 7,
     name: 'CALZONE',
     price: 95,
     pizzagroup: 3,
   },
   {
+    id: 8,
     name: 'CALZONE KEBAB',
     price: 100,
     pizzagroup: 3,
   },
   {
+    id: 9,
     name: 'MILANO SPECIAL',
     price: 100,
     pizzagroup: 3,
   },
   {
+    id: 10,
     name: 'PALERMO',
     price: 100,
     pizzagroup: 4,
   },
   {
+    id: 11,
     name: 'BAGAREN SPECIAL',
     price: 100,
     pizzagroup: 4,
   },
   {
+    id: 12,
     name: 'KEBABPIZZA',
     price: 100,
     pizzagroup: 5,
   },
   {
+    id: 13,
     name: 'HOCKEYPIZZA',
     price: 105,
     pizzagroup: 5,
   },
   {
+    id: 14,
     name: 'FAVORIT',
     price: 100,
     pizzagroup: 5,
